7d4ceda50fd82e5a9a8a43d05d4677638666c375
Emmanuwil <simonemmanuwil@gmail.com>
2021-08-28 18:09:17 -0400

    First Commit

---
+++ Operators/Operators.js
@@ -0,0 +1,14 @@
+let a = 20
+let b = 4
+
+ let add = a + b;
+ console.log(add); // output -> 24
+
+ let minus = a - b; 
+ console.log(minus); // output -> 16
+
+ let multiply = a * b;
+ console.log(multiply); // output -> 80
+
+ let deviding = a / b;
+ console.log(deviding); // output -> 5
