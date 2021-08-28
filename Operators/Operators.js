0432d30ac873f87200ee47a9cf6b794bb9f2ca5f
Emmanuwil <simonemmanuwil@gmail.com>
2021-08-28 18:51:12 -0400

    Second Commit

--- Operators/Operators.js
+++ Operators/Operators.js
@@ -1,3 +1,4 @@
+//Exercise 1
 let a = 20
 let b = 4
 
@@ -12,3 +13,32 @@ let b = 4
 
  let deviding = a / b;
  console.log(deviding); // output -> 5
+
+
+//Exercise 2
+
+let c = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+ 
+console.log(c + str); // Output -> 1111
+console.log(c + str2); // Output -> 11eleven
+console.log(c + isPresent); // Output -> 12
+console.log(c + firstName); // Output -> 11Jackkie
+console.log(c + lastName); // 11Chan
+
+//Exercise 3
+
+ let d = 5;
+ let str3 = "5";
+ let str4 = "five";
+ let isPresent2 = false;
+ let firstName2 = "Robin";
+ let lastName2 = "Williams";
+
+ console.log(d == str3); // true
+ console.log(d === str4); // fasle
+ console.log(!isPresent2); // true
